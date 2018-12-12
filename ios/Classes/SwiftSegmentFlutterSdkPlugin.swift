6882bd83c7180d7d1200ac8b91944ec28017ec72
Jon Duran <jonyas@gmail.com>
2018-12-12 13:33:20 +0200

    Add dummy iOS implementation for event handling

--- ios/Classes/SwiftSegmentFlutterSdkPlugin.swift
+++ ios/Classes/SwiftSegmentFlutterSdkPlugin.swift
@@ -3,12 +3,24 @@ import UIKit
 
 public class SwiftSegmentFlutterSdkPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "segment_flutter_sdk", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "net.freska.fluttersegmentsdk/segment_flutter_sdk", binaryMessenger: registrar.messenger())
     let instance = SwiftSegmentFlutterSdkPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "init":
+        NSLog("init called, Missing iOS implementation")
+        result(nil)
+    case "trackScreen":
+        NSLog("trackScreen called, Missing iOS implementation")
+        result(nil)
+    case "trackEvent":
+        NSLog("trackEvent called, Missing iOS implementation")
+        result(nil)
+    default:
+        result(FlutterMethodNotImplemented)
+    }
   }
 }
